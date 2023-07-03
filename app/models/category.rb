79efd1ba5cc1caee800f87e86ebc08f89c900c0c
yodit93 <yoditabebe9391@gmail.com>
2023-07-03 18:36:04 +0300

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_many :category_transfers, dependent: :destroy
+  has_many :transfers, through: :category_transfers
   validates :name, presence: true
   validates :icon, presence: true
 end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,5 +1,7 @@
 class Transfer < ApplicationRecord
   belongs_to :user
+  has_many :category_transfers, dependent: :destroy
+  has_many :categories, through: :category_transfers
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0.1 }
 end
