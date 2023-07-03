610247a99a0911d03f2551d0fe803ff1caff8513
yodit93 <yoditabebe9391@gmail.com>
2023-07-03 18:23:04 +0300

    Add test for transfermodel validations

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Transfer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    Transfer.destroy_all
+    User.destroy_all
+    @user = User.create(name: 'user', email: 'user@gmail.com', password: '123456', confirmed_at: Time.current)
+    sign_in @user
+    @transfer = Transfer.create(name: 'transfer', amount: 100, user: @user)
+  end
+
+  it 'should name be present' do
+    @transfer.name = nil
+    expect(@transfer).to be_valid
+  end
+
+  it 'should amount be present' do
+    @transfer.amount = nil
+    expect(@transfer).to be_valid
+  end
+
+  it 'should amount be numeric and greater than or equal to 0.1' do
+    @transfer.amount = 'abc'
+    expect(@transfer).to be_valid
+  end
 end
