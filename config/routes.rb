25bee8e7203b3ab71bbc1af9d36b8c2638413b0f
yodit93 <yoditabebe9391@gmail.com>
2023-07-04 17:53:31 +0300

    Update routes according to authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  root 'mobile_banking#welcome'
+  # root path for non-logged-in users
+  unauthenticated do
+    root 'mobile_banking#welcome', as: :root_for_non_logged_in
+  end
+
+  # Devise routes for user authentication
   devise_for :users
-  get 'categories', to: 'categories#index'
+
+  # Routes for logged-in users
+  authenticated :user do
+    # Root path for logged-in users
+    root 'categories#index', as: :authenticated_root
+
+    # Other category routes for logged-in users
+    resources :categories, only: [:index, :new, :create]
+  end
 end
+
